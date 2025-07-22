c2b017728fb6e9f54287c6df1bcc59ae3121a6ce
Maksik <maxylover@mail.ru>
2025-07-22 22:42:40 +0300

    Изменил структуру проекта, перенёс суперклас в пакет Animals, поправил нахождение важных файлов в проекте таких как .gitignor.

--- src/Animals/Animal.java
+++ src/Animals/Animal.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 
 public abstract class Animal {
-    protected int id;
+    protected static int id;
     protected String name;
     protected LocalDate birthDate;
     protected List<String> commands;
